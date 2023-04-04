8d11905a4c69892b7fdb2f7722303b9418ed605a
joaaoinacio <joaaoinacio@gmail.com>
2023-04-04 14:05:43 -0300

    Memoteca

---
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,29 @@
+import { PensamentoService } from './../pensamento.service';
+import { Pensamento } from './../pensamento/pensamento';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.css']
+})
+export class ListarPensamentosComponent implements OnInit {
+
+  listarPensamentos: Pensamento[] = [];
+
+
+  constructor (private service: PensamentoService) { }
+
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listarPensamentos) => {
+      this.listarPensamentos = listarPensamentos
+    })
+
+  }
+
+
+
+
+
+}
